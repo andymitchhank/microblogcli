a7fef90ef092ec7cad703fe00038e61c73435ec4
Andrew Henry <andymitchhank@users.noreply.github.com>
2018-01-11 14:25:17 -0600

    Prompt for post if not provided

--- microblogcli/cli/post.py
+++ microblogcli/cli/post.py
@@ -6,12 +6,15 @@ from ..models import Account
 
 @click.command()
 @click.option('--title', '-t', default=None)
-@click.argument('content')
+@click.argument('content', required=False, default=None)
 def post(title, content):
 	""" Create a new post on your Micro.blog 
 
 		This only works for hosted Micro.blog accounts for now. 
 	"""
+	if not content:
+		content = click.prompt('', type=str)
+
 
 	create_post(Account.current_token(), content, title)
 	
