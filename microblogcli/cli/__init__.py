cc8ba948840e7e32371109f4ba63760fb6bbb6f4
Andrew Henry <andymitchhank@users.noreply.github.com>
2018-01-11 14:25:52 -0600

    Link up new commands

--- microblogcli/cli/__init__.py
+++ microblogcli/cli/__init__.py
@@ -4,6 +4,8 @@ from click import echo
 from ..models import create_tables
 from .account import account
 from .post import post
+from .feed import feed
+from .users import follow, unfollow, following, am_following
 
 
 create_tables()
@@ -16,3 +18,8 @@ def cli():
 
 cli.add_command(account)
 cli.add_command(post)
+cli.add_command(feed)
+cli.add_command(follow)
+cli.add_command(unfollow)
+cli.add_command(following)
+cli.add_command(am_following)
